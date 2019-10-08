5135e85c239ae26366a89e07a5ab833b7bc3aefb
Lazar Yasinskiy <lazar1999yasinskiy@gmail.com>
2019-10-08 15:02:34 +0300

    адаптировал меню, настроил слайдер

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-  $('.slider').slick({
-    
+  $('.header__slider').slick({
+    arrows: false,
+    dots:true,
+    cssEase: 'cubic-bezier(.03,.02,.5,.99)',
   });
 });
